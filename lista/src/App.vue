4b8ab43977f185dc0705a84bd1618977ff2b204d
Hamilton Rodrigues <hamiltonventtura@gmail.com>
2024-05-02 16:04:20 -0300

    Projeto

---
+++ lista/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <h1> Bem vindos primeiro Teste com Vue.jS</h1>
+  <h3>{{ dados.toLocaleUpperCase()}}</h3>
+
+  <ul>
+    <li><a href="#">Início</a></li>
+    <li><a href="#">Cadastro</a></li>
+  </ul>
+  <HelloWorld/>
+  <GerenciaProjeto/>
+
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import GerenciaProjeto from './components/GerenciaProjeto.vue';
+
+export default {
+  components:
+  {
+    HelloWorld,
+    GerenciaProjeto
+  },
+  data() {
+      return {
+        dados: "bom dia!"
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
+
+
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Bem Vindo a Lista de Tarefas"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
---
+++ lista/src/components/GerenciaProjeto.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="GerenciaProjeto">
+    <h1>Cadastro de Tarefas</h1>
+    <input v-model="novaTarefa" placeholder="Digite uma nova tarefa">
+    <button @click="adicionarTarefa">Adicionar</button>
+
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index">
+        {{ tarefa }}
+        <button @click="removerTarefa(index)">Remover</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GerenciaProjeto',
+  data() {
+    return {
+      novaTarefa: '',
+      tarefas: []
+    }
+  },
+  methods: {
+    adicionarTarefa() {
+      if (this.novaTarefa.trim() !== '') {
+        this.tarefas.push(this.novaTarefa);
+        this.novaTarefa = '';
+      }
+    },
+    removerTarefa(index) {
+      this.tarefas.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
